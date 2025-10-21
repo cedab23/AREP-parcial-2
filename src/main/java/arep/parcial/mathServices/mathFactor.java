29261823084660b26bedfb55414614bd5af21ad1
unknown <nicolas.pachon-u@labinfo.is.escuelaing.edu.co>
2025-10-21 10:28:33 -0500

    updated: methods and readme

--- src/main/java/arep/parcial/mathServices/mathFactor.java
+++ src/main/java/arep/parcial/mathServices/mathFactor.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package arep.parcial.mathServices;
 
-/**
- *
- * @author nicolas.pachon-u
- */
+
 public class mathFactor {
     
+    private int[] list; 
+    
+    public int[] factor(int number){
+    int i=1;    
+     while(i<=number){
+         if((number%i)==0){
+             //list.add(i);
+         }
+         i++;
+     }
+     
+     return list;
+    
+}
 }
--- src/main/java/arep/parcial/mathServices/mathPrime.java
+++ src/main/java/arep/parcial/mathServices/mathPrime.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package arep.parcial.mathServices;
 
-/**
- *
- * @author nicolas.pachon-u
- */
+
+
+//metodo encargado de las operaciones asociadas a numeros primos
 public class mathPrime {
+    
+    private int[] list; 
+    
+    public int[] prime(int number){
+    int i=1;    
+    int j=1;
+    int count=0;
+     while(i<=number){
+         count=0;
+         while(j<=i){
+             if((j%i)==0){
+             count++;
+             }
+             j++;
+         }
+         if(count>2){
+         }else{
+         //list.add(i);
+         }
+         i++;
+     }
+    
+    return list;
+    }
     
 }
