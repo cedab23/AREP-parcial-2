c9c7e64db36c22a5ff690171808b7adc37c08bfa
unknown <nicolas.pachon-u@labinfo.is.escuelaing.edu.co>
2025-10-21 09:08:53 -0500

    add: spring boot environment

---
+++ src/test/java/arep/parcial/mathServices/MathServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package arep.parcial.mathServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
